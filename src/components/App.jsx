c3eb67a7f76d7ebad6abde6db812d7f561afde18
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-06-08 12:22:02 +0300

    small refactor to object in useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,50 @@ import { FeedbackOptions } from './FeedbackOptions';
 import { Statistics } from './Statistics';
 
 export function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const temporaryObject = { good, neutral, bad };
+  // Спочатку зробив окремі стані, для кожного значення, в яких зберігав примітив
+  // Але, прийшлося збирати в тимчасовий об'єкт, щоб передати масив у FeedbackOptions
 
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+  // const temporaryObject = { good, neutral, bad };
+
+  // const onLeaveFeedback = option => {
+  //   switch (option) {
+  //     case 'good':
+  //       setGood(good + 1);
+  //       break;
+  //     case 'neutral':
+  //       setNeutral(neutral + 1);
+  //       break;
+  //     case 'bad':
+  //       setBad(bad + 1);
+  //       break;
+  //     default:
+  //       console.log('Something goes wrong');
+  //   }
+  // };
+
+  // const countTotalFeedback = () => {
+  //   return [good, neutral, bad].reduce((acc, el) => {
+  //     return acc + el;
+  //   }, 0);
+  // };
+
+  // А тут переробив стан на об'єкт, і працює. Але чи коректно так робити, саме в
+  // даній задачі?
+
+  const [myState, mySetState] = useState({ good: 0, neutral: 0, bad: 0 });
   const onLeaveFeedback = option => {
     switch (option) {
       case 'good':
-        setGood(good + 1);
+        mySetState(prevState => ({ ...prevState, good: myState.good + 1 }));
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        mySetState(prevState => ({ ...prevState, neutral: myState.neutral + 1 }));
         break;
       case 'bad':
-        setBad(bad + 1);
+        mySetState(prevState => ({ ...prevState, bad: myState.bad + 1 }));
         break;
       default:
         console.log('Something goes wrong');
@@ -26,28 +55,28 @@ export function App() {
   };
 
   const countTotalFeedback = () => {
-    return [good, neutral, bad].reduce((acc, el) => {
+    return Object.values(myState).reduce((acc, el) => {
       return acc + el;
     }, 0);
   };
 
-  const countPositiveFeedbackPercentage = () => {
+  const countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
     return ((good / (neutral + bad)) * 100).toFixed() + '%';
   };
 
   const total = countTotalFeedback();
-  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage(myState);
 
   return (
     <MainSection>
-      <FeedbackOptions array={Object.keys(temporaryObject)} onLeaveFeedback={onLeaveFeedback} />
+      <FeedbackOptions array={Object.keys(myState)} onLeaveFeedback={onLeaveFeedback} />
       {total === 0 ? (
         <NotificationMessage>There is no feedback</NotificationMessage>
       ) : (
         <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
+          good={myState.good}
+          neutral={myState.neutral}
+          bad={myState.bad}
           total={total}
           avarage={positiveFeedbackPercentage}
         />
